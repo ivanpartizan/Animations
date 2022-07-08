3431050a075375db2591d751160f7cf375a495e3
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-07-08 19:05:50 +0200

    update

--- Page Transitions/app.js
+++ Page Transitions/app.js
@@ -1,3 +1,68 @@
+const tlLeave = gsap.timeline({
+  defaults: { duration: 0.75, ease: "Power2.easeOut" },
+});
+const tlEnter = gsap.timeline({
+  defaults: { duration: 0.75, ease: "Power2.easeOut" },
+});
+
+const leaveAnimation = (current, done) => {
+  const product = current.querySelector(".image-container");
+  const text = current.querySelector(".showcase-text");
+  const circles = current.querySelectorAll(".circle");
+  const arrow = current.querySelector(".showcase-arrow");
+  return (
+    tlLeave.fromTo(arrow, { opacity: 1, y: 0 }, { opacity: 0, y: 50 }),
+    tlLeave.fromTo(
+      product,
+      { y: 0, opacity: 1 },
+      { y: 100, opacity: 0, onComplete: done },
+      "<"
+    ),
+    tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<"),
+    tlLeave.fromTo(
+      circles,
+      { y: 0, opacity: 1 },
+      {
+        y: -200,
+        opacity: 0,
+        stagger: 0.15,
+        ease: "back.out(1.7)",
+        duration: 1,
+      },
+      "<"
+    )
+  );
+};
+
+const enterAnimation = (current, done) => {
+  const product = current.querySelector(".image-container");
+  const text = current.querySelector(".showcase-text");
+  const circles = current.querySelectorAll(".circle");
+  const arrow = current.querySelector(".showcase-arrow");
+  return (
+    tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+    tlLeave.fromTo(
+      product,
+      { y: -100, opacity: 0 },
+      { y: 0, opacity: 1, onComplete: done },
+      "<"
+    ),
+    tlLeave.fromTo(text, { y: 100, opacity: 0 }, { opacity: 1, y: 0 }, "<"),
+    tlLeave.fromTo(
+      circles,
+      { y: -200, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.15,
+        ease: "back.out(1.7)",
+        duration: 1,
+      },
+      "<"
+    )
+  );
+};
+
 barba.init({
   preventRunning: true,
   transitions: [
@@ -6,21 +71,12 @@ barba.init({
       leave(data) {
         const done = this.async();
         let current = data.current.container;
-        gsap.fromTo(
-          current,
-          { opacity: 1 },
-          { opacity: 0, duration: 1, onComplete: done }
-        );
+        leaveAnimation(current, done);
       },
       enter(data) {
         const done = this.async();
-
         let next = data.next.container;
-        gsap.fromTo(
-          next,
-          { opacity: 0 },
-          { opacity: 1, duration: 1, onComplete: done }
-        );
+        enterAnimation(next, done);
       },
     },
   ],

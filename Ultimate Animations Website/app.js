aa0b7fd24c09f41e5a799632d0cf3ec767d27a84
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-07-16 23:47:14 +0200

    update

--- Ultimate Animations Website/app.js
+++ Ultimate Animations Website/app.js
@@ -46,3 +46,87 @@ const tlHRemove = gsap.timeline({
 });
 
 tlHRemove.to(".highlight", { color: "rgba(255, 255, 255, 0.4)", stagger: 1 });
+
+const tlSplit = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-page",
+    start: "-25%",
+    end: "30%",
+    markers: true,
+    scrub: true,
+  },
+});
+
+tlSplit.fromTo(".large-phone", { x: "40%" }, { x: "20%" });
+tlSplit.fromTo(".small-phone", { x: "-40%" }, { x: "-20%" }, "<");
+tlSplit.fromTo(
+  ".product-text-left",
+  { x: 50, opacity: 0 },
+  { x: 0, opacity: 1 },
+  "<"
+);
+tlSplit.fromTo(
+  ".product-text-right",
+  { x: -50, opacity: 0 },
+  { x: 0, opacity: 1 },
+  "<"
+);
+
+const tlSplitPin = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third-page",
+    pin: true,
+    pinSpacing: false,
+    start: "0%",
+    end: "100%",
+  },
+});
+
+// Carousel
+const swatches = document.querySelectorAll(".swatches img");
+const gallery = document.querySelector(".phone-gallery");
+const slides = document.querySelectorAll(".phone-gallery-container");
+
+let currentSwatch = "blue";
+let topIndex = 2;
+
+swatches.forEach((swatch, index) => {
+  const coord = slides[index].getBoundingClientRect().left;
+
+  swatch.addEventListener("click", (e) => {
+    let swatchName = e.target.getAttribute("swatch");
+    let closeUp = document.querySelector("." + swatchName);
+
+    if (currentSwatch === swatchName) return;
+
+    gsap.set(closeUp, { zIndex: topIndex });
+    gsap.fromTo(closeUp, { opacity: 0 }, { opacity: 1, duration: 1 });
+
+    gsap.to(gallery, { x: -coord, duration: 1, ease: "back.out(1)" });
+
+    topIndex++;
+    currentSwatch = swatchName;
+  });
+});
+
+const tlVideo = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".fifth-page",
+    start: "0%",
+    end: "150%",
+    scrub: true,
+    pin: true,
+  },
+});
+
+tlVideo.fromTo(
+  ".product-video",
+  { currentTime: 0 },
+  { currentTime: 3, duration: 1 }
+);
+tlVideo.fromTo(
+  ".product-info-container h3",
+  { opacity: 0 },
+  { opacity: 1, stagger: 0.25, duration: 0.5 },
+  "<"
+);

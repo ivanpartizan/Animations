c383b650f43e7938ca03182d2d22e1aed59ccd5d
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-07-17 23:12:14 +0200

    update

--- Ultimate Animations Website/app.js
+++ Ultimate Animations Website/app.js
@@ -130,3 +130,16 @@ tlVideo.fromTo(
   { opacity: 1, stagger: 0.25, duration: 0.5 },
   "<"
 );
+
+const tlParallax = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".sixth-page",
+    start: "-25%",
+    end: "50%",
+    scrub: true,
+  },
+});
+
+tlParallax.fromTo(".photo-description", { y: 0 }, { y: -80 });
+tlParallax.fromTo(".portrait-container", { y: 0 }, { y: -80 }, "<");
+tlParallax.fromTo(".phone-video", { y: 0 }, { y: -200 }, "<");
